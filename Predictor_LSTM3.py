90ed35307bc3003048989a5df54c3bf1642da401
neiltej <107909422+neiltej@users.noreply.github.com>
2022-10-27 01:05:23 +0800

    Update Predictor_LSTM3.py

--- Predictor_LSTM3.py
+++ Predictor_LSTM3.py
@@ -42,7 +42,21 @@ def main():
         elif prediction > 0.5:
             st.success('The email is spam')
             
+    
+    
+    
+    st.header("The Need for Spam Detection")
+    st.text("""
+        Spam detection algorithms are used to detect and filter junk and spam emails with a high level of accuracy.
+        It is said that around half of all emails are spam, depending on the user. These emails can include scams or viruses intended to cause harm.
+        """)
 
+    st.header("Data Source")
+    st.text("""
+        Data Source: Preprocessed TREC 2007 Public Corpus Dataset. 
+        Link: https://www.kaggle.com/datasets/imdeepmind/preprocessed-trec-2007-public-corpus-dataset
+        
+        """)
 
 if __name__ == "__main__":
     main()
